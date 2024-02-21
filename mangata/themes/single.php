09449dd1a68494ce7e4b18450fcada450b188e91
rosiestrose17 <114959784+rosiestrose17@users.noreply.github.com>
2024-02-21 06:54:22 -0500

    Update single.php

--- mangata/themes/single.php
+++ mangata/themes/single.php
@@ -1,11 +1,16 @@
 <?php get_header(); ?>
- <h1>
-  <?php echo get_the_title(); ?>
-</h1>
-<div class="content">
-  <?php
-    echo get_the_content();
-  ?>
-</div>
+
+<?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+        <h1><?php the_title(); ?></h1>
+        <div class="content">
+            <?php the_content(); ?>
+        </div>
+    <?php endwhile; ?>
+<?php else : ?>
+    <p>No content found</p>
+<?php endif; ?>
+
 <p>This is a single.php template</p>
+
 <?php get_footer(); ?>
