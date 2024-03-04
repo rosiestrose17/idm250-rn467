592a9cc2644d4f9dcd4ecf58311cadb2b0600745
rosiestrose17 <114959784+rosiestrose17@users.noreply.github.com>
2024-03-04 04:06:14 -0500

    Create video-embed.php

---
+++ mangata/themes/component/video-embed.php
@@ -0,0 +1,6 @@
+<div class="video-container">
+    <?php
+    $iframe_src = "https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F1680770725755304%2F&show_text=false&width=317&t=0";
+    ?>
+    <iframe src="<?php echo $iframe_src; ?>" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+</div>
