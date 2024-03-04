2ef4c048bb90d59acefc1c639335b36d083a5cd7
rosiestrose17 <114959784+rosiestrose17@users.noreply.github.com>
2024-03-04 04:10:38 -0500

    Create text-image.php

---
+++ mangata/themes/component/text-image.php
@@ -0,0 +1,22 @@
+  <div class="image-container">
+            <img src="<?php echo get_template_directory_uri(); ?>/images/letter.png" alt="Image">
+            </div>
+            <br>
+ <div class="extra-info">
+  <div class="extra-content">
+    <h2 class="wp-block-heading">Heading 2</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <h3 class="wp-block-heading">Heading 3</h3>
+    <ul class="unordered-list">
+      <li>Unordered list item 1</li>
+      <li>Unordered list item 2</li>
+      <li>Unordered list item 3</li>
+      <li>Unordered list item 4</li>
+      <li>Unordered list item 5</li>
+    </ul>
+    </div>
+<figure class="wp-block-image">
+    <img src="https://rosiestrose.com/idm250/wp-content/themes/portfolio-2023/images/bunny.png" alt="Size guide"> 
+        <figcaption>IMAGE WITH CAPTION</figcaption>
+    </figure>
+</div>
