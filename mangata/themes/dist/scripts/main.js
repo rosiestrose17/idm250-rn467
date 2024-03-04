dbdd65c26000fab2b2c0fe74a78cb3ee529c2adf
rosiestrose17 <114959784+rosiestrose17@users.noreply.github.com>
2024-03-04 04:14:29 -0500

    Create main.js

---
+++ mangata/themes/dist/scripts/main.js
@@ -0,0 +1,9 @@
+ function toggleMenu() {
+            var menu = document.querySelector('.menu');
+            menu.classList.toggle('show'); 
+        }
+
+        function toggleSearch() {
+            var searchForm = document.querySelector('.search-form');
+            searchForm.classList.toggle('show'); 
+        }
