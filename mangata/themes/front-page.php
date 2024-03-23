2cc1db497b0ef24d72e8918987679ec79e24b502
rosiestrose17 <114959784+rosiestrose17@users.noreply.github.com>
2024-03-23 01:41:25 -0400

    Update front-page.php

--- mangata/themes/front-page.php
+++ mangata/themes/front-page.php
@@ -1,13 +1,8 @@
 <?php get_header(); ?>
-<div class="body-container">
-<h1 class="">
-  <?php echo get_the_title(); ?>
-</h1>
-<div class="content">
-  <?php
+  <div class="content">
+    <?php
     echo get_the_content();
-  ?>
-</div>
-<p>This is a front-page.php template</p>
-</div>
+    ?>
+      </div>
 <?php get_footer(); ?>
+
