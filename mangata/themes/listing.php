98436b22ce6585e15c83482078946742cc7ff2bd
rosiestrose17 <114959784+rosiestrose17@users.noreply.github.com>
2024-03-23 01:48:37 -0400

    Add files via upload

---
+++ mangata/themes/listing.php
@@ -0,0 +1,12 @@
+
+<?php
+/*
+Template Name: Blog Listing Template
+*/
+get_header(); ?>
+  <div class="content">
+    <?php
+    echo get_the_content();
+    ?>
+      </div>
+<?php get_footer(); ?>
